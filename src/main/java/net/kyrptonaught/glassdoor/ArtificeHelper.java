99c47a1349af449b26cabfee710114b2d33588f1
kyrptonaught <anthony.butillo10@gmail.com>
2019-07-14 15:13:27 -0400

    Update dependencies Fixes villagers not using vanilla doors(Fabric bug)

--- src/main/java/net/kyrptonaught/glassdoor/ArtificeHelper.java
+++ src/main/java/net/kyrptonaught/glassdoor/ArtificeHelper.java
@@ -2,15 +2,20 @@ package net.kyrptonaught.glassdoor;
 
 import com.swordglowsblue.artifice.api.Artifice;
 import com.swordglowsblue.artifice.api.ArtificeResourcePack;
-import com.swordglowsblue.artifice.api.resource.StringResource;
+import com.swordglowsblue.artifice.api.resource.TemplateResource;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import java.io.*;
+import java.util.stream.Collectors;
+
 class ArtificeHelper {
     static void generateRecipies() {
-        String[] woodtype = {"oak", "spruce", "birch", "acacia", "jungle", "dark_oak" , "iron"};
+        new TemplateResource();
+        String[] woodtype = {"oak", "spruce", "birch", "acacia", "jungle", "dark_oak", "iron"};
         Artifice.registerData("", pack -> {
             registerRecipe(pack, "glassoak", ModBlocks.oak_glassdoor, Blocks.OAK_DOOR);
             registerRecipe(pack, "glassspruce", ModBlocks.spruce_glassdoor, Blocks.SPRUCE_DOOR);
@@ -18,9 +23,10 @@ class ArtificeHelper {
             registerRecipe(pack, "glassbirch", ModBlocks.birch_glassdoor, Blocks.BIRCH_DOOR);
             registerRecipe(pack, "glassjungle", ModBlocks.jungle_glassdoor, Blocks.JUNGLE_DOOR);
             registerRecipe(pack, "glassdarkoak", ModBlocks.dark_oak_glassdoor, Blocks.DARK_OAK_DOOR);
-            registerRecipe(pack,"glassiron", ModBlocks.iron_glassdoor,Blocks.IRON_DOOR);
+            registerRecipe(pack, "glassiron", ModBlocks.iron_glassdoor, Blocks.IRON_DOOR);
+            String templateFile = readTemplateFile("loot_door");
             for (String wood : woodtype)
-                pack.add(new Identifier(GlassDoorMod.MOD_ID, "loot_tables/blocks/" + wood + "_glassdoor.json"), new StringResource(generateLootTable(wood)));
+                pack.add(new Identifier(GlassDoorMod.MOD_ID, "loot_tables/blocks/" + wood + "_glassdoor.json"), new TemplateResource(templateFile).expand("woodtype", wood));
         });
     }
 
@@ -35,8 +41,10 @@ class ArtificeHelper {
     static void generateAssets() {
         String[] woodtype = {"oak", "spruce", "birch", "acacia", "jungle", "dark_oak", "iron"};
         Artifice.registerAssets(GlassDoorMod.MOD_ID + ":glassdoors", pack -> {
+            String templateFile = readTemplateFile("blockstate_door");
             for (String wood : woodtype) {
-                pack.add(new Identifier(GlassDoorMod.MOD_ID, "blockstates/" + wood + "_glassdoor.json"), new StringResource(generateBlockState(wood)));
+                pack.add(new Identifier(GlassDoorMod.MOD_ID, "blockstates/" + wood + "_glassdoor.json"), new TemplateResource(templateFile).expand("woodtype", wood));
+
                 pack.addItemModel(new Identifier(GlassDoorMod.MOD_ID, wood + "_glassdoor"), model -> {
                     model.parent(new Identifier("item/generated"));
                     model.texture("layer0", new Identifier(GlassDoorMod.MOD_ID, "item/" + wood + "_door"));
@@ -65,73 +73,10 @@ class ArtificeHelper {
         });
     }
 
-    static String generateLootTable(String wood) {
-        return "{\n" +
-                "  \"type\": \"minecraft:block\",\n" +
-                "  \"pools\": [\n" +
-                "    {\n" +
-                "      \"rolls\": 1,\n" +
-                "      \"entries\": [\n" +
-                "        {\n" +
-                "          \"type\": \"minecraft:item\",\n" +
-                "          \"conditions\": [\n" +
-                "            {\n" +
-                "              \"condition\": \"minecraft:block_state_property\",\n" +
-                "              \"block\": \"glassdoor:" + wood + "_glassdoor\",\n" +
-                "              \"properties\": {\n" +
-                "                \"half\": \"lower\"\n" +
-                "              }\n" +
-                "            }\n" +
-                "          ],\n" +
-                "          \"name\": \"glassdoor:" + wood + "_glassdoor\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"conditions\": [\n" +
-                "        {\n" +
-                "          \"condition\": \"minecraft:survives_explosion\"\n" +
-                "        }\n" +
-                "      ]\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-    }
-
-    static String generateBlockState(String wood) {
-        return ("{\n" +
-                "    \"variants\": {\n" +
-                "        \"facing=east,half=lower,hinge=left,open=false\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom\" },\n" +
-                "        \"facing=south,half=lower,hinge=left,open=false\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom\", \"y\": 90 },\n" +
-                "        \"facing=west,half=lower,hinge=left,open=false\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom\", \"y\": 180 },\n" +
-                "        \"facing=north,half=lower,hinge=left,open=false\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom\", \"y\": 270 },\n" +
-                "        \"facing=east,half=lower,hinge=right,open=false\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom_hinge\" },\n" +
-                "        \"facing=south,half=lower,hinge=right,open=false\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom_hinge\", \"y\": 90 },\n" +
-                "        \"facing=west,half=lower,hinge=right,open=false\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom_hinge\", \"y\": 180 },\n" +
-                "        \"facing=north,half=lower,hinge=right,open=false\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom_hinge\", \"y\": 270 },\n" +
-                "        \"facing=east,half=lower,hinge=left,open=true\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom_hinge\", \"y\": 90 },\n" +
-                "        \"facing=south,half=lower,hinge=left,open=true\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom_hinge\", \"y\": 180 },\n" +
-                "        \"facing=west,half=lower,hinge=left,open=true\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom_hinge\", \"y\": 270 },\n" +
-                "        \"facing=north,half=lower,hinge=left,open=true\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom_hinge\" },\n" +
-                "        \"facing=east,half=lower,hinge=right,open=true\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom\", \"y\": 270 },\n" +
-                "        \"facing=south,half=lower,hinge=right,open=true\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom\" },\n" +
-                "        \"facing=west,half=lower,hinge=right,open=true\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom\", \"y\": 90 },\n" +
-                "        \"facing=north,half=lower,hinge=right,open=true\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_bottom\", \"y\": 180 },\n" +
-                "        \"facing=east,half=upper,hinge=left,open=false\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top\" },\n" +
-                "        \"facing=south,half=upper,hinge=left,open=false\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top\", \"y\": 90 },\n" +
-                "        \"facing=west,half=upper,hinge=left,open=false\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top\", \"y\": 180 },\n" +
-                "        \"facing=north,half=upper,hinge=left,open=false\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top\", \"y\": 270 },\n" +
-                "        \"facing=east,half=upper,hinge=right,open=false\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top_hinge\" },\n" +
-                "        \"facing=south,half=upper,hinge=right,open=false\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top_hinge\", \"y\": 90 },\n" +
-                "        \"facing=west,half=upper,hinge=right,open=false\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top_hinge\", \"y\": 180 },\n" +
-                "        \"facing=north,half=upper,hinge=right,open=false\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top_hinge\", \"y\": 270 },\n" +
-                "        \"facing=east,half=upper,hinge=left,open=true\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top_hinge\", \"y\": 90 },\n" +
-                "        \"facing=south,half=upper,hinge=left,open=true\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top_hinge\", \"y\": 180 },\n" +
-                "        \"facing=west,half=upper,hinge=left,open=true\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top_hinge\", \"y\": 270 },\n" +
-                "        \"facing=north,half=upper,hinge=left,open=true\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top_hinge\" },\n" +
-                "        \"facing=east,half=upper,hinge=right,open=true\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top\", \"y\": 270 },\n" +
-                "        \"facing=south,half=upper,hinge=right,open=true\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top\" },\n" +
-                "        \"facing=west,half=upper,hinge=right,open=true\":  { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top\", \"y\": 90 },\n" +
-                "        \"facing=north,half=upper,hinge=right,open=true\": { \"model\": \"" + GlassDoorMod.MOD_ID + ":block/" + wood + "_door_top\", \"y\": 180 }\n" +
-                "    }\n" +
-                "}\n");
+    private static String readTemplateFile(String templateFile) {
+        String file =  "/template/" + templateFile + ".json";
+        InputStream in = GlassDoorMod.class.getResourceAsStream(file);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        return reader.lines().collect(Collectors.joining());
     }
 }
